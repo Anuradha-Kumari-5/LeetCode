f842319239f5047a4e8ba0e51d4f3bb50df82c0a
Anuradha-Kumari-5 <anuradha110104@gmail.com>
2026-09-09 02:01:16 +0530

    Time: 2 ms (89.12%) | Memory: 48 MB (42.49%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int currentReach = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(i>currentReach){
+                return false;
+            }
+            currentReach = Math.max(currentReach, i + nums[i]);
+            if(currentReach >= nums.length - 1){
+                return true;
+            }
+            
+        }
+        return true;
+    }
+}
